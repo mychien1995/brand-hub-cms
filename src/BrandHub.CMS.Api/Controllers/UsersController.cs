f441e28c5d2139e5d554f272b5686c57f3755f16
chien.tran <mychien1995@gmail.com>
2019-10-28 21:57:46 +0700

    Create user base

---
+++ src/BrandHub.CMS.Api/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using BrandHub.CMS.Api.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrandHub.CMS.Api.Controllers
+{
+    [Route("cms/api/[controller]")]
+    [ApiController]
+    [FrameworkAuthorized]
+
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> Create()
+        {
+            return 1;
+        }
+    }
+}
---
+++ src/BrandHub.CMS.Api/Models/CreateUserModel.cs
@@ -0,0 +1,14 @@
+﻿using BrandHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrandHub.CMS.Api.Models
+{
+    public class CreateUserModel : UserModel
+    {
+        public int? OrganizationId { get; set; }
+        public int Role { get; set; }
+    }
+}
