8e2388f96a21a3ad085a2b5aa80489487bd5f97b
chien.tran <mychien1995@gmail.com>
2019-10-28 20:53:01 +0700

    Update database scripts

--- scripts/Full_script.sql
+++ scripts/Full_script.sql
@@ -1,15 +1,4 @@
-USE [BrandHub]
-GO
-/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-/****** Object:  Table [dbo].[Addresses]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+-- 28/10/2019 ---
 CREATE TABLE [dbo].[Addresses](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[AddressLine] [nvarchar](max) NULL,
@@ -20,14 +9,8 @@ CREATE TABLE [dbo].[Addresses](
  CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Countries]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[Countries](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[CountryCode] [nvarchar](max) NULL,
@@ -35,14 +18,8 @@ CREATE TABLE [dbo].[Countries](
  CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Districts]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[Districts](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -50,14 +27,8 @@ CREATE TABLE [dbo].[Districts](
  CONSTRAINT [PK_Districts] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[HostDefinitions]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[HostDefinitions](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[OrganizationId] [int] NOT NULL,
@@ -65,14 +36,8 @@ CREATE TABLE [dbo].[HostDefinitions](
  CONSTRAINT [PK_HostDefinitions] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[OrganizationRoles]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[OrganizationRoles](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -80,14 +45,8 @@ CREATE TABLE [dbo].[OrganizationRoles](
  CONSTRAINT [PK_OrganizationRoles] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Organizations]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[Organizations](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -99,14 +58,8 @@ CREATE TABLE [dbo].[Organizations](
  CONSTRAINT [PK_Organizations] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[OrganizationUsers]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[OrganizationUsers](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[OrganizationId] [int] NOT NULL,
@@ -115,14 +68,8 @@ CREATE TABLE [dbo].[OrganizationUsers](
  CONSTRAINT [PK_OrganizationUsers] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Provinces]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[Provinces](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](max) NULL,
@@ -130,28 +77,16 @@ CREATE TABLE [dbo].[Provinces](
  CONSTRAINT [PK_Provinces] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Roles]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[Roles](
 	[ID] [int] NOT NULL,
 	[RoleName] [nvarchar](max) NULL,
  CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[UserRoles]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[UserRoles](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[UserId] [int] NOT NULL,
@@ -159,14 +94,8 @@ CREATE TABLE [dbo].[UserRoles](
  CONSTRAINT [PK_UserRoles] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Users]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[Users](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Username] [nvarchar](max) NULL,
@@ -184,14 +113,8 @@ CREATE TABLE [dbo].[Users](
  CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[UserTokens]    Script Date: 10/28/2019 8:31:56 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+));
+
 CREATE TABLE [dbo].[UserTokens](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Value] [nvarchar](max) NULL,
@@ -201,166 +124,76 @@ CREATE TABLE [dbo].[UserTokens](
  CONSTRAINT [PK_UserTokens] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Addresses_CountryId]    Script Date: 10/28/2019 8:31:56 PM ******/
+));
+
 CREATE NONCLUSTERED INDEX [IX_Addresses_CountryId] ON [dbo].[Addresses]
 (
 	[CountryId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Addresses_DistrictId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_Addresses_DistrictId] ON [dbo].[Addresses]
 (
 	[DistrictId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Addresses_ProvinceId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_Addresses_ProvinceId] ON [dbo].[Addresses]
 (
 	[ProvinceId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Districts_ProvinceId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_Districts_ProvinceId] ON [dbo].[Districts]
 (
 	[ProvinceId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_HostDefinitions_OrganizationId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_HostDefinitions_OrganizationId] ON [dbo].[HostDefinitions]
 (
 	[OrganizationId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_OrganizationRoles_OrganizationId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_OrganizationRoles_OrganizationId] ON [dbo].[OrganizationRoles]
 (
 	[OrganizationId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Organizations_AddressId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_Organizations_AddressId] ON [dbo].[Organizations]
 (
 	[AddressId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Organizations_ProvinceID]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_Organizations_ProvinceID] ON [dbo].[Organizations]
 (
 	[ProvinceID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_OrganizationUsers_OrganizationId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_OrganizationUsers_OrganizationId] ON [dbo].[OrganizationUsers]
 (
 	[OrganizationId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_OrganizationUsers_RoleId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_OrganizationUsers_RoleId] ON [dbo].[OrganizationUsers]
 (
 	[RoleId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_OrganizationUsers_UserId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_OrganizationUsers_UserId] ON [dbo].[OrganizationUsers]
 (
 	[UserId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_Provinces_CountryId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_Provinces_CountryId] ON [dbo].[Provinces]
 (
 	[CountryId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_UserRoles_RoleId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_UserRoles_RoleId] ON [dbo].[UserRoles]
 (
 	[RoleId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-/****** Object:  Index [IX_UserRoles_UserId]    Script Date: 10/28/2019 8:31:56 PM ******/
+);
 CREATE NONCLUSTERED INDEX [IX_UserRoles_UserId] ON [dbo].[UserRoles]
 (
 	[UserId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Addresses]  WITH CHECK ADD  CONSTRAINT [FK_Addresses_Countries_CountryId] FOREIGN KEY([CountryId])
-REFERENCES [dbo].[Countries] ([ID])
-GO
-ALTER TABLE [dbo].[Addresses] CHECK CONSTRAINT [FK_Addresses_Countries_CountryId]
-GO
-ALTER TABLE [dbo].[Addresses]  WITH CHECK ADD  CONSTRAINT [FK_Addresses_Districts_DistrictId] FOREIGN KEY([DistrictId])
-REFERENCES [dbo].[Districts] ([ID])
-GO
-ALTER TABLE [dbo].[Addresses] CHECK CONSTRAINT [FK_Addresses_Districts_DistrictId]
-GO
-ALTER TABLE [dbo].[Addresses]  WITH CHECK ADD  CONSTRAINT [FK_Addresses_Provinces_ProvinceId] FOREIGN KEY([ProvinceId])
-REFERENCES [dbo].[Provinces] ([ID])
-GO
-ALTER TABLE [dbo].[Addresses] CHECK CONSTRAINT [FK_Addresses_Provinces_ProvinceId]
-GO
-ALTER TABLE [dbo].[Districts]  WITH CHECK ADD  CONSTRAINT [FK_Districts_Provinces_ProvinceId] FOREIGN KEY([ProvinceId])
-REFERENCES [dbo].[Provinces] ([ID])
-GO
-ALTER TABLE [dbo].[Districts] CHECK CONSTRAINT [FK_Districts_Provinces_ProvinceId]
-GO
-ALTER TABLE [dbo].[HostDefinitions]  WITH CHECK ADD  CONSTRAINT [FK_HostDefinitions_Organizations_OrganizationId] FOREIGN KEY([OrganizationId])
-REFERENCES [dbo].[Organizations] ([ID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[HostDefinitions] CHECK CONSTRAINT [FK_HostDefinitions_Organizations_OrganizationId]
-GO
-ALTER TABLE [dbo].[OrganizationRoles]  WITH CHECK ADD  CONSTRAINT [FK_OrganizationRoles_Organizations_OrganizationId] FOREIGN KEY([OrganizationId])
-REFERENCES [dbo].[Organizations] ([ID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[OrganizationRoles] CHECK CONSTRAINT [FK_OrganizationRoles_Organizations_OrganizationId]
-GO
-ALTER TABLE [dbo].[Organizations]  WITH CHECK ADD  CONSTRAINT [FK_Organizations_Addresses_AddressId] FOREIGN KEY([AddressId])
-REFERENCES [dbo].[Addresses] ([ID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[Organizations] CHECK CONSTRAINT [FK_Organizations_Addresses_AddressId]
-GO
-ALTER TABLE [dbo].[Organizations]  WITH CHECK ADD  CONSTRAINT [FK_Organizations_Provinces_ProvinceID] FOREIGN KEY([ProvinceID])
-REFERENCES [dbo].[Provinces] ([ID])
-GO
-ALTER TABLE [dbo].[Organizations] CHECK CONSTRAINT [FK_Organizations_Provinces_ProvinceID]
-GO
-ALTER TABLE [dbo].[OrganizationUsers]  WITH CHECK ADD  CONSTRAINT [FK_OrganizationUsers_OrganizationRoles_RoleId] FOREIGN KEY([RoleId])
-REFERENCES [dbo].[OrganizationRoles] ([ID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[OrganizationUsers] CHECK CONSTRAINT [FK_OrganizationUsers_OrganizationRoles_RoleId]
-GO
-ALTER TABLE [dbo].[OrganizationUsers]  WITH CHECK ADD  CONSTRAINT [FK_OrganizationUsers_Organizations_OrganizationId] FOREIGN KEY([OrganizationId])
-REFERENCES [dbo].[Organizations] ([ID])
-GO
-ALTER TABLE [dbo].[OrganizationUsers] CHECK CONSTRAINT [FK_OrganizationUsers_Organizations_OrganizationId]
-GO
-ALTER TABLE [dbo].[OrganizationUsers]  WITH CHECK ADD  CONSTRAINT [FK_OrganizationUsers_Users_UserId] FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([ID])
-GO
-ALTER TABLE [dbo].[OrganizationUsers] CHECK CONSTRAINT [FK_OrganizationUsers_Users_UserId]
-GO
-ALTER TABLE [dbo].[Provinces]  WITH CHECK ADD  CONSTRAINT [FK_Provinces_Countries_CountryId] FOREIGN KEY([CountryId])
-REFERENCES [dbo].[Countries] ([ID])
-GO
-ALTER TABLE [dbo].[Provinces] CHECK CONSTRAINT [FK_Provinces_Countries_CountryId]
-GO
-ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY([RoleId])
-REFERENCES [dbo].[Roles] ([ID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_Roles_RoleId]
-GO
-ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([ID])
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_Users_UserId]
-GO
+);
+ALTER TABLE [dbo].[Addresses] ADD  CONSTRAINT [FK_Addresses_Countries_CountryId] FOREIGN KEY([CountryId]) REFERENCES [dbo].[Countries] ([ID]);
+ALTER TABLE [dbo].[Addresses] ADD  CONSTRAINT [FK_Addresses_Districts_DistrictId] FOREIGN KEY([DistrictId]) REFERENCES [dbo].[Districts] ([ID]);
+ALTER TABLE [dbo].[Addresses] ADD  CONSTRAINT [FK_Addresses_Provinces_ProvinceId] FOREIGN KEY([ProvinceId]) REFERENCES [dbo].[Provinces] ([ID]);
+ALTER TABLE [dbo].[Districts] ADD  CONSTRAINT [FK_Districts_Provinces_ProvinceId] FOREIGN KEY([ProvinceId]) REFERENCES [dbo].[Provinces] ([ID]);
+ALTER TABLE [dbo].[HostDefinitions]  ADD  CONSTRAINT [FK_HostDefinitions_Organizations_OrganizationId] FOREIGN KEY([OrganizationId]) REFERENCES [dbo].[Organizations] ([ID]) ON DELETE CASCADE;
+ALTER TABLE [dbo].[OrganizationRoles]  ADD  CONSTRAINT [FK_OrganizationRoles_Organizations_OrganizationId] FOREIGN KEY([OrganizationId]) REFERENCES [dbo].[Organizations] ([ID]) ON DELETE CASCADE;
+ALTER TABLE [dbo].[Organizations]  ADD  CONSTRAINT [FK_Organizations_Addresses_AddressId] FOREIGN KEY([AddressId]) REFERENCES [dbo].[Addresses] ([ID]) ON DELETE CASCADE;
+ALTER TABLE [dbo].[Organizations]  ADD  CONSTRAINT [FK_Organizations_Provinces_ProvinceID] FOREIGN KEY([ProvinceID]) REFERENCES [dbo].[Provinces] ([ID]);
+ALTER TABLE [dbo].[OrganizationUsers] ADD  CONSTRAINT [FK_OrganizationUsers_OrganizationRoles_RoleId] FOREIGN KEY([RoleId]) REFERENCES [dbo].[OrganizationRoles] ([ID]) ON DELETE CASCADE;
+ALTER TABLE [dbo].[OrganizationUsers]  ADD  CONSTRAINT [FK_OrganizationUsers_Organizations_OrganizationId] FOREIGN KEY([OrganizationId]) REFERENCES [dbo].[Organizations] ([ID]);
+ALTER TABLE [dbo].[OrganizationUsers]  ADD  CONSTRAINT [FK_OrganizationUsers_Users_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[Users] ([ID]);
+ALTER TABLE [dbo].[Provinces] ADD  CONSTRAINT [FK_Provinces_Countries_CountryId] FOREIGN KEY([CountryId]) REFERENCES [dbo].[Countries] ([ID]);
+ALTER TABLE [dbo].[UserRoles] ADD  CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY([RoleId]) REFERENCES [dbo].[Roles] ([ID]) ON DELETE CASCADE;
+ALTER TABLE [dbo].[UserRoles]  ADD  CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[Users] ([ID]) ON DELETE CASCADE;
+-- 28/10/2019 ---
