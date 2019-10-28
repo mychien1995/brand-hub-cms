c32376cca93a2669d675c4d6440ede947b432055
Chien1995 <mychien1995@gmail.com>
2019-10-28 18:12:55 +0700

    access token service

--- src/BrandHub.Data.EF/Repositories/Users/UserTokenRepository.cs
+++ src/BrandHub.Data.EF/Repositories/Users/UserTokenRepository.cs
@@ -17,6 +17,7 @@ namespace BrandHub.Data.EF.Repositories.Users
     public interface IUserTokenRepository : IEntityRepository<ApplicationUserToken>
     {
         string GetToken(int userId, string userName);
+        bool IsValidToken(string token, string username);
     }
 
     [ServiceTypeOf(typeof(IUserTokenRepository))]
@@ -29,24 +30,39 @@ namespace BrandHub.Data.EF.Repositories.Users
             _appSettingReader = appSettingReader;
             TokenDurationInMinutes = _appSettingReader.GetValue<int>(AuthenticationConstants.TokenDurationKey, 30);
         }
-
+        public bool IsValidToken(string token, string username)
+        {
+            if (string.IsNullOrWhiteSpace(token)
+                || string.IsNullOrWhiteSpace(username))
+            {
+                return false;
+            }
+            var encryptedToken = EncryptUtils.SHA256Encrypt(token, username);
+            var existingToken = this.GetQueryable().AsNoTracking().FirstOrDefault(x => x.Value == encryptedToken);
+            if (existingToken == null) return false;
+            if (existingToken.ExpiredTime < DateTime.UtcNow)
+            {
+                Delete(existingToken);
+                SaveChanges();
+                return false;
+            }
+            return true;
+        }
         public string GetToken(int userId, string userName)
         {
             var now = DateTime.UtcNow;
             var existingToken = this.GetQueryable().AsNoTracking().FirstOrDefault(x => x.UserId == userId);
             if (existingToken != null)
             {
-                if (existingToken.ExpiredTime > now)
-                    return existingToken.Value;
                 this.Delete(existingToken.ID);
             }
             var randomToken = EncryptUtils.GenerateAccessToken();
-            //var encryptedToken = EncryptUtils.SHA256Encrypt(randomToken, userName);
+            var encryptedToken = EncryptUtils.SHA256Encrypt(randomToken, userName);
             var token = new ApplicationUserToken()
             {
                 StartedTime = now,
                 UserId = userId,
-                Value = randomToken,
+                Value = encryptedToken,
                 ExpiredTime = now.AddMinutes(TokenDurationInMinutes)
             };
             this.Insert(token);
--- src/BrandHub.Services/Services/Authentication/AccessTokenService.cs
+++ src/BrandHub.Services/Services/Authentication/AccessTokenService.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using BrandHub.Data.EF.Repositories.Users;
+using BrandHub.Framework.IoC;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BrandHub.Services.Authentication
 {
-    class AccessTokenService
+    public interface IAccessTokenService
     {
+        string GetToken(int userId, string userName);
+        bool IsValidToken(string token, string username);
+    }
+
+    [ServiceTypeOf(typeof(IAccessTokenService))]
+    public class AccessTokenService : IAccessTokenService
+    {
+        private readonly IUserTokenRepository _userTokenRepository;
+        public AccessTokenService(IUserTokenRepository userTokenRepository)
+        {
+            _userTokenRepository = userTokenRepository;
+        }
+        public string GetToken(int userId, string userName)
+        {
+            return _userTokenRepository.GetToken(userId, userName);
+        }
+
+        public bool IsValidToken(string token, string username)
+        {
+            return _userTokenRepository.IsValidToken(token, username);
+        }
     }
 }
