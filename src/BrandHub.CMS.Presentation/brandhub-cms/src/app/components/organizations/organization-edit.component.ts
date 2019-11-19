4af523bed91a3e525fd80c0cf548b111ba49899d
Chien1995 <mychien1995@gmail.com>
2019-11-19 17:57:38 +0700

    Routing is real

--- src/BrandHub.CMS.Presentation/brandhub-cms/src/app/components/organizations/organization-edit.component.ts
+++ src/BrandHub.CMS.Presentation/brandhub-cms/src/app/components/organizations/organization-edit.component.ts
@@ -3,7 +3,7 @@ import { BaseComponent } from '../base.component';
 import { ConfigService } from '../../services/shared/config.service';
 import { ClientService } from '../../services/shared/client.service';
 import { OrganizationModel } from '../../models/organizations/organization.model';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-organization-edit',
@@ -14,18 +14,25 @@ export class OrganizationEditComponent extends BaseComponent implements OnInit {
 
   private model: OrganizationModel;
   private isEdit: boolean;
+  private id: number;
   constructor() {
     super();
+    this._router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        this.id = ev.id;
+        if (this.id) {
+          this.isEdit = true;
+        }
+      }
+    });
     this.model = new OrganizationModel();
-    var id = this._activatedRoute.snapshot.paramMap.get('id');
-    if (id) this.isEdit = true;
   }
 
   ngOnInit() {
     if (this.isEdit) {
-      var id = this._activatedRoute.snapshot.paramMap.get('id');
+      var id = this.id;
       var url = this._configService.getEndpoint(`organization/${id}`);
-      this._clientService.get<OrganizationModel>(url).subscribe(data => this.model = data);
+      this._clientService.get < OrganizationModel > (url).subscribe(data => this.model = data);
     } else {
       this.model = new OrganizationModel();
     }
